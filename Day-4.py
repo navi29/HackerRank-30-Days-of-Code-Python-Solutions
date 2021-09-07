b4012c24a2eec2245ec3a3b9acb64b4fb0795fd7
Navya Sri Jampa <navyasri.jampa@gmail.com>
2021-09-07 14:41:47 +0530

    Add files via upload

---
+++ Day-4.py
@@ -0,0 +1,29 @@
+class Person:
+
+    def __init__(self, intialAge):
+        if intialAge > 0:
+            self.age = intialAge
+        else:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+
+    def yearPasses(self):
+        self.age += 1
+
+    def amIOld(self):
+        if self.age<13:
+            print("You are young.")
+        elif self.age>=13 and self.age<18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
+t = int(input())
+for i in range(t):
+    age = int(input())
+    p = Person(age)
+    p.amIOld()
+    for j in range(3):
+        p.yearPasses()
+    p.amIOld()
+    print("")
