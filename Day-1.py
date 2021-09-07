910e0306e7a695ebce655acacb2e6567357a8361
Navya Sri Jampa <navyasri.jampa@gmail.com>
2021-09-07 15:28:21 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,27 @@
+#---------------------------------------------
+#Problem Statement:  https://www.hackerrank.com/challenges/30-data-types/problem
+#---------------------------------------------
+#Language: Python
+#---------------------------------------------
+#Solution:
+#---------------------------------------------
+
+# Declare second integer, double, and String variables.
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+integer = int(input())
+float_number = float(input())
+string = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+integer)
+
+# Print the sum of the double variables on a new line.
+print(d+float_number)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+string)
